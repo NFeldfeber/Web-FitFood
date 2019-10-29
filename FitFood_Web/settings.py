bda2cd37b38ab9c4e2d96e9e7b80f3e7d92418c7
Nicolas Feldfeber <nicofeldfeberdesarrollador@gmail.com>
2019-10-28 22:00:18 -0300

    Setup to deploy

--- FitFood_Web/settings.py
+++ FitFood_Web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@s)_r#h($^h7!pot9bluydk%(hgc^%whjh=b18j^k@u2fn3oj&'
+SECRET_KEY = os.environ.get('SITE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
