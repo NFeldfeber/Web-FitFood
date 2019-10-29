bfb1a10174ee29710e32c44642aefc15c0ba7d82
Nicolas Feldfeber <nicofeldfeberdesarrollador@gmail.com>
2019-10-28 22:34:37 -0300

    To Deploy

--- FitFood_Web/settings.py
+++ FitFood_Web/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@s)_r#h($^h7!pot9bluydk%(hgc^%whjh=b18j^k@u2fn3oj&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
